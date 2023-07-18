c3e56dac45a60a55e895a2e1c1f23221494eff1d
xiaoshihou <onelessononeexercise@gmail.com>
2023-07-18 10:13:11 +0800

    feat(api): support setting formatters and linters for multiple filetypes in one go
    Resolves #14
    But it basically doubles the code in filetype.lua, so I'm not sure if it should be merged.

--- lua/guard/filetype.lua
+++ lua/guard/filetype.lua
@@ -35,8 +35,49 @@ local function box()
   return setmetatable({}, tbl)
 end
 
-return setmetatable(M, {
+
+local function box_for_group(fts)
+  for _, ft in pairs(fts) do
+    if not rawget(M, ft) then
+      rawset(M, ft, box())
+    end
+  end
+  local current
+  local tbl = {}
+  tbl.__index = tbl
+
+  function tbl:fmt(config)
+    for _, ft in pairs(self) do
+      M[ft]:fmt(config)
+    end
+    current = 'format'
+    return self
+  end
+
+  function tbl:append(val)
+    for _, ft in pairs(self) do
+      local opt = M[ft][current]
+      opt[#opt + 1] = val
+    end
+    return self
+  end
+
+  function tbl:lint(config)
+    for _, ft in pairs(self) do
+      M[ft]:lint(config)
+    end
+    current = 'linter'
+    return self
+  end
+
+  return setmetatable(fts, tbl)
+end
+
+M = setmetatable(M, {
   __call = function(t, ft)
+    if type(ft) == "table" then
+      return box_for_group(ft)
+    end
     if not rawget(t, ft) then
       rawset(t, ft, box())
     end
