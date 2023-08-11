f8f09aa5525b242a5ee3e07ea6ad95ef763c71f3
Barrett Ruth <br.barrettruth@gmail.com>
2023-08-10 19:40:35 -0500

    feat(format): more tools and remove defaults

--- lua/guard/tools/formatter.lua
+++ lua/guard/tools/formatter.lua
@@ -6,10 +6,63 @@ M.lsp = {
   end,
 }
 
+M.black = {
+  cmd = 'black',
+  args = { '-' },
+  stdin = true,
+}
+
+M.cbfmt = {
+  cmd = 'cbfmt',
+  args = { '--best-effort', '--stdin-filepath' },
+  stdin = true,
+  fname = true,
+}
+
 M['clang-format'] = {
   cmd = 'clang-format',
-  args = { '-style=file' },
   stdin = true,
+}
+
+M.djhtml = {
+  cmd = 'djhtml',
+  args = { '-' },
+  stdin = true
+}
+
+M.fish_indent = {
+  cmd = 'fish_indent',
+  stdin = true,
+}
+
+M.gofmt = {
+  cmd = 'gofmt',
+  stdin = true,
+}
+
+M.golines = {
+  cmd = 'golines',
+  args = { '--max-len=80' },
+  stdin = true,
+}
+
+M['google-java-format'] = {
+  cmd = 'google-java-format',
+  args = { '-' },
+  stdin = true,
+}
+
+M.isort = {
+  cmd = 'isort',
+  args = { '-' },
+  stdin = true,
+}
+
+M.mixformat = {
+  cmd = 'mix',
+  args = {'format', '-', '--stdin-filename'},
+  stdin = true,
+  fname = true,
 }
 
 M.prettier = {
@@ -17,6 +70,13 @@ M.prettier = {
   args = { '--stdin-filepath' },
   fname = true,
   stdin = true,
+}
+
+M.prettierd = {
+  cmd = 'prettierd',
+  args = { '--stdin-filepath' },
+  stdin = true,
+  fname = true,
 }
 
 M.rubocop = {
@@ -32,9 +92,8 @@ M.rustfmt = {
   stdin = true,
 }
 
-M.golines = {
-  cmd = 'golines',
-  args = { '--max-len=80' },
+M.shfmt = {
+  cmd = 'shfmt',
   stdin = true,
 }
 
@@ -44,74 +103,14 @@ M.stylua = {
   stdin = true,
 }
 
-M.black = {
-  cmd = 'black',
-  args = { '-' },
+M.swiftformat = {
+  cmd = 'swiftformat',
   stdin = true,
-}
-
-M.mixformat = {
-  cmd = 'mix',
-  args = {'format', '-', '--stdin-filename'},
-  stdin = true,
-  fname = true,
-}
-
-M.djhtml = {
-  cmd = 'djhtml',
-  args = { '-' },
-  stdin = true
-}
-
-M.cbfmt = {
-  cmd = 'cbfmt',
-  args = { '--best-effort', '--stdin-filepath' },
-  stdin = true,
-  fname = true
-}
-
-M.shfmt = {
-  cmd = 'shfmt',
-  stdin = true,
-}
-
-M.isort = {
-  cmd = 'isort',
-  args = { '-' },
-  stdin = true
-}
-
-M.prettierd = {
-  cmd = 'prettierd',
-  args = { '--stdin-filepath' },
-  stdin = true,
-  fname = true
 }
 
 M['sql-formatter'] = {
   cmd = 'sql-formatter',
-  stdin = true
-}
-
-M.fish_indent = {
-  cmd = 'fish_indent',
-  stdin = true
-}
-
-M.swiftformat = {
-  cmd = 'swiftformat',
-  stdin = true
-}
-
-M.gofmt = {
-  cmd = 'gofmt',
-  stdin = true
-}
-
-M['google-java-format'] = {
-  cmd = 'google-java-format',
-  args = { '-' },
-  stdin = true
+  stdin = true,
 }
 
 return M
