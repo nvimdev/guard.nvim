94e1ae638abfa7989117b4b015d8801bebec90c2
xiaoshihou514 <108414369+xiaoshihou514@users.noreply.github.com>
2023-08-13 13:03:24 +0800

    chore: fix ci tests again (#56)
    * chore: fix tests again
    
    a commit changed clang-format's default so the :extra test fails??
    
    * changed test case

--- lua/guard/events.lua
+++ lua/guard/events.lua
@@ -16,31 +16,31 @@ end
 
 local function create_lspattach_autocmd(fmt_on_save)
   api.nvim_create_autocmd('LspAttach', {
-      group = group,
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        ---@diagnostic disable-next-line: need-check-nil
-        if not client.supports_method('textDocument/formatting') then
-          return
-        end
-        local ft = vim.bo[args.buf].filetype
-        if not(ft_handler[ft] and ft_handler[ft].format) then
-          ft_handler(ft):fmt('lsp')
-        end
+    group = group,
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      ---@diagnostic disable-next-line: need-check-nil
+      if not client.supports_method('textDocument/formatting') then
+        return
+      end
+      local ft = vim.bo[args.buf].filetype
+      if not (ft_handler[ft] and ft_handler[ft].format) then
+        ft_handler(ft):fmt('lsp')
+      end
 
-        if
-          fmt_on_save
-          and #api.nvim_get_autocmds({
-              group = group,
-              event = 'FileType',
-              pattern = ft,
-            })
-            == 0
-        then
-          format.attach_to_buf(args.buf)
-        end
-      end,
-    })
+      if
+        fmt_on_save
+        and #api.nvim_get_autocmds({
+            group = group,
+            event = 'FileType',
+            pattern = ft,
+          })
+          == 0
+      then
+        format.attach_to_buf(args.buf)
+      end
+    end,
+  })
 end
 
 return {
