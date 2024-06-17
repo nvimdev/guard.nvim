3c1caffd64c31a74318ed03256796582c7cb706a
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-06-17 11:47:36 +0100

    test: toolcopy

--- test/format_spec.lua
+++ test/format_spec.lua
@@ -2,16 +2,14 @@
 local api = vim.api
 local equal = assert.equal
 local ft = require('guard.filetype')
-ft('lua'):fmt({
-  cmd = 'stylua',
-  args = { '-' },
-  stdin = true,
-})
-require('guard').setup()
 
 describe('format module', function()
   local bufnr
   before_each(function()
+    for k, _ in pairs(ft) do
+      ft[k] = nil
+    end
+
     bufnr = api.nvim_create_buf(true, false)
     vim.bo[bufnr].filetype = 'lua'
     api.nvim_set_current_buf(bufnr)
@@ -19,8 +17,12 @@ describe('format module', function()
   end)
 
   it('can format with stylua', function()
-    vim.bo[bufnr].filetype = 'lua'
-    api.nvim_set_current_buf(bufnr)
+    ft('lua'):fmt({
+      cmd = 'stylua',
+      args = { '-' },
+      stdin = true,
+    })
+    require('guard').setup()
     api.nvim_buf_set_lines(bufnr, 0, -1, false, {
       'local a',
       '          = "test"',
@@ -29,5 +31,21 @@ describe('format module', function()
     vim.wait(500)
     local line = api.nvim_buf_get_lines(bufnr, 0, -1, false)[1]
     equal([[local a = 'test']], line)
+  end)
+
+  it('can format with function', function()
+    ft('lua'):fmt({
+      fn = function(buf, range, acc)
+        return table.concat(vim.split(acc, '\n'), '') .. vim.inspect(range)
+      end,
+    })
+    api.nvim_buf_set_lines(bufnr, 0, -1, false, {
+      'local a',
+      '          = "test"',
+    })
+    require('guard.format').do_fmt(bufnr)
+    vim.wait(500)
+    local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    assert.are.same({ 'local a          = "test"nil' }, lines)
   end)
 end)
