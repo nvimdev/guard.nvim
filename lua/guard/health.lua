dcb2ee0497ec7c791e092ec0e1933f99dd32c73a
xiaoshihou <xiaoshihou@tutamail.com>
2025-06-08 10:02:32 +0100

    chore: add custom health check capabilities

--- lua/guard/health.lua
+++ lua/guard/health.lua
@@ -5,28 +5,32 @@ local util = require('guard.util')
 local events = require('guard.events')
 local M = {}
 
+local function check_cmd(conf, checked)
+  if conf.cmd and not vim.tbl_contains(checked, conf.cmd) then
+    if fn.executable(conf.cmd) == 1 then
+      ok(conf.cmd .. ' found')
+    else
+      error(conf.cmd .. ' not found')
+    end
+    table.insert(checked, conf.cmd)
+  end
+end
+
 local function executable_check()
   local checked = {}
   for _, item in pairs(filetype) do
     for _, conf in ipairs(item.formatter or {}) do
-      if conf.cmd and not vim.tbl_contains(checked, conf.cmd) then
-        if fn.executable(conf.cmd) == 1 then
-          ok(conf.cmd .. ' found')
-        else
-          error(conf.cmd .. ' not found')
-        end
-        table.insert(checked, conf.cmd)
+      if conf.health then
+        conf.health()
+      else
+        check_cmd(conf, checked)
       end
     end
-
     for _, conf in ipairs(item.linter or {}) do
-      if conf.cmd and not vim.tbl_contains(checked, conf.cmd) then
-        if fn.executable(conf.cmd) == 1 then
-          ok(conf.cmd .. ' found')
-        else
-          error(conf.cmd .. ' not found')
-        end
-        table.insert(checked, conf.cmd)
+      if conf.health then
+        conf.health()
+      else
+        check_cmd(conf, checked)
       end
     end
   end
