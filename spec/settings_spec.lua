5af5231ba264c2a162349a642dc757ecd746849b
SebasF1349 <ratablanca711@gmail.com>
2026-01-17 12:24:24 -0300

    fix: tests

--- spec/settings_spec.lua
+++ spec/settings_spec.lua
@@ -150,7 +150,7 @@ describe('settings', function()
     same(true, util.getopt('auto_lint'))
     vim.cmd('silent! write!')
     vim.wait(500)
-    same('Very important error message[error code 114514]', vd.get()[1].message)
+    same('Very important error message', vd.get()[1].message)
 
     vim.g.guard_config = { auto_lint = false }
     same(false, util.getopt('auto_lint'))
