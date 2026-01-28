cc011d854af3ebfaf29208b31a41c35176b9d0a5
tar <private@mothzarella.dev>
2026-01-28 14:39:46 +0100

    feat: Update provider fn signature to receive fname and cwd
    closes #246

--- lua/guard/lint.lua
+++ lua/guard/lint.lua
@@ -117,7 +117,7 @@ function M.do_lint_single(buf, config)
       end)
     end)
   else
-    data = lint.fn(prev_lines)
+    data = lint.fn(prev_lines, fname, cwd)
     if #data > 0 then
       results = lint.parse(data, buf, fname, cwd)
     end
